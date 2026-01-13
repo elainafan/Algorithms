eb0a2fec10016bc140880a2030da7b2f7563438c
elainafan <869812580@qq.com>
2026-01-13 20:47:27 +0800

    update monotone stack

--- 数据结构/Monotone stack.cpp
+++ 数据结构/Monotone stack.cpp
@@ -7,13 +7,13 @@ int largestRectangleArea(vector<int>& heights) {
         while (!s.empty() && heights[s.top()] >= heights[i]) s.pop();
         if (!s.empty()) r[i] = s.top();
         s.push(i);
-    }
+    } // 求右边第一个小于的下标
     while (!s.empty()) s.pop();
     for (int i = 0; i <= n - 1; i++) {
         while (!s.empty() && heights[s.top()] >= heights[i]) s.pop();
         if (!s.empty()) l[i] = s.top();
         s.push(i);
-    }
+    } // 求左边第一个小于的下标
     int maxx = INT_MIN;
     for (int i = 0; i <= n - 1; i++) maxx = max(maxx, heights[i] * (r[i] - l[i] - 1));
     return maxx;
