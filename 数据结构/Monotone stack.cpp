f311b04b60a3ba44b6f2e648ade30307e3371dcb
elainafan <869812580@qq.com>
2026-01-14 08:49:38 +0800

    update monotone stack

--- 数据结构/Monotone stack.cpp
+++ 数据结构/Monotone stack.cpp
@@ -1,20 +1,20 @@
-int largestRectangleArea(vector<int>& heights) {
-    int n = heights.size();
+int largestRectangleArea(vector<int>& nums) {
+    int n = nums.size();
     vector<int> r(n, n);
     vector<int> l(n, -1);
     stack<int> s;
     for (int i = n - 1; i >= 0; i--) {
-        while (!s.empty() && heights[s.top()] >= heights[i]) s.pop();
+        while (!s.empty() && nums[s.top()] >= nums[i]) s.pop();
         if (!s.empty()) r[i] = s.top();
         s.push(i);
     } // 求右边第一个小于的下标
     while (!s.empty()) s.pop();
     for (int i = 0; i <= n - 1; i++) {
-        while (!s.empty() && heights[s.top()] >= heights[i]) s.pop();
+        while (!s.empty() && nums[s.top()] >= nums[i]) s.pop();
         if (!s.empty()) l[i] = s.top();
         s.push(i);
     } // 求左边第一个小于的下标
     int maxx = INT_MIN;
-    for (int i = 0; i <= n - 1; i++) maxx = max(maxx, heights[i] * (r[i] - l[i] - 1));
+    for (int i = 0; i <= n - 1; i++) maxx = max(maxx, nums[i] * (r[i] - l[i] - 1));
     return maxx;
 }
