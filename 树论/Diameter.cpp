6a45fd7959e72a8c1a5cf6f1ed7b784fcd1742b7
elainafan <869812580@qq.com>
2026-01-29 10:50:23 +0800

    update diameter

---
+++ 树论/Diameter.cpp
@@ -0,0 +1,14 @@
+int ans = 0;
+auto dfs = [&](this auto&& dfs, int x, int pa) -> int {
+    int tem = 0;
+    for (int& p : ma[x]) {
+        if (p == pa) continue;
+        int tem2 = dfs(p, x) + 1;
+        // if (s[x] == s[p]) continue;
+        ans = max(ans, tem + tem2);
+        tem = max(tem, tem2);
+    }
+    return tem;
+};
+dfs(0, -1);
+return ans + 1;
