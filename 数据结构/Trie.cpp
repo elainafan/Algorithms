31032b58dc3ef302d97a2b35ae9ab348747c25f2
elainafan <869812580@qq.com>
2026-02-02 22:54:40 +0800

    update trie

--- 数据结构/Trie.cpp
+++ 数据结构/Trie.cpp
@@ -1,6 +1,7 @@
 struct Node {
     Node* son[26]{};
-    bool end = false;
+    int pre = 0;
+    int endcnt = 0;
 };
 class Trie {
     Node* root = new Node();
@@ -12,19 +13,35 @@ class Trie {
             if (!cur->son[c]) return 0;
             cur = cur->son[c];
         }
-        return cur->end ? 2 : 1;
+        return cur->endcnt > 0 ? 2 : 1;
     }
     string findpre(string word) {
         Node* cur = root;
-        int cnt = 0;
-        for (char c : word) {
-            if (cur->end) return word.substr(0, cnt);
-            c -= 'a';
-            cnt++;
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i] - 'a';
             if (!cur->son[c]) return "";
             cur = cur->son[c];
+            if (cur->endcnt > 0) return word.substr(0, i + 1);
         }
         return "";
+    }
+    int findequal(string word) {
+        Node* cur = root;
+        for (char c : word) {
+            c -= 'a';
+            if (!cur->son[c]) return 0;
+            cur = cur->son[c];
+        }
+        return cur->endcnt;
+    }
+    int findprefix(string prefix) {
+        Node* cur = root;
+        for (char c : prefix) {
+            c -= 'a';
+            if (!cur->son[c]) return 0;
+            cur = cur->son[c];
+        }
+        return cur->pre;
     }
     void destory(Node* node) {
         if (!node) return;
@@ -39,12 +56,14 @@ public:
     // 往trie中插入word
     void insert(string word) {
         Node* cur = root;
+        cur->pre++;
         for (char c : word) {
             c -= 'a';
             if (!cur->son[c]) cur->son[c] = new Node();
             cur = cur->son[c];
+            cur->pre++;
         }
-        cur->end = true;
+        cur->endcnt++;
     }
 
     // 查询trie中是否有word的前缀，若有返回，若无返回空串
@@ -55,4 +74,22 @@ public:
 
     // 查询是否存在以prefix为前缀的字符串
     bool startsWith(string prefix) { return find(prefix) != 0; }
+
+    // 查询插入了与word相同的数量
+    int cntword(string word) { return findequal(word); }
+
+    // 查询插入了以prefix为前缀的数量
+    int cntprefix(string prefix) { return findprefix(prefix); }
+
+    // 往trie中删除word
+    void erase(string word) {
+        Node* cur = root;
+        cur->pre--;
+        for (char c : word) {
+            c -= 'a';
+            cur = cur->son[c];
+            cur->pre--;
+        }
+        cur->endcnt--;
+    }
 };
