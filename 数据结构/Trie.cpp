65546b8950f8cab112bce0782c9281beb14f601c
elainafan <869812580@qq.com>
2026-02-03 09:54:46 +0800

    update trie

--- 数据结构/Trie.cpp
+++ 数据结构/Trie.cpp
@@ -93,3 +93,90 @@ public:
         cur->endcnt--;
     }
 };
+
+// 下面是非字母版的
+
+struct Node {
+    unordered_map<int, Node*> son;
+    int pre = 0;
+    int endcnt = 0;
+};
+class Trie {
+    Node* root = new Node();
+
+    int find(const vector<int>& a) {
+        Node* cur = root;
+        for (auto x : a) {
+            if (!cur->son.count(x)) return 0;
+            cur = cur->son[x];
+        }
+        return cur->endcnt > 0 ? 2 : 1;
+    }
+
+    vector<int> findpre(const vector<int>& a) {
+        Node* cur = root;
+        for (int i = 0; i < a.size(); i++) {
+            auto x = a[i];
+            if (!cur->son.count(x)) return {};
+            cur = cur->son[x];
+            if (cur->endcnt > 0) return vector<int>(a.begin(), a.begin() + i + 1);
+        }
+        return {};
+    }
+
+    int findequal(const vector<int>& a) {
+        Node* cur = root;
+        for (auto x : a) {
+            if (!cur->son.count(x)) return 0;
+            cur = cur->son[x];
+        }
+        return cur->endcnt;
+    }
+
+    int findprefix(const vector<int>& pre) {
+        Node* cur = root;
+        for (auto x : pre) {
+            if (!cur->son.count(x)) return 0;
+            cur = cur->son[x];
+        }
+        return cur->pre;
+    }
+
+    void destroy(Node* node) {
+        if (!node) return;
+        for (auto& [_, son] : node->son) destroy(son);
+        delete node;
+    }
+
+public:
+    ~Trie() { destroy(root); }
+
+    void insert(const vector<int>& a) {
+        Node* cur = root;
+        cur->pre++;
+        for (auto x : a) {
+            if (!cur->son.count(x)) cur->son[x] = new Node();
+            cur = cur->son[x];
+            cur->pre++;
+        }
+        cur->endcnt++;
+    }
+
+    bool search(const vector<int>& a) { return find(a) == 2; }
+
+    bool startsWith(const vector<int>& pre) { return find(pre) != 0; }
+
+    int cntword(const vector<int>& a) { return findequal(a); }
+
+    int cntprefix(const vector<int>& pre) { return findprefix(pre); }
+
+    void erase(const vector<int>& a) {
+        Node* cur = root;
+        cur->pre--;
+        for (auto x : a) {
+            cur = cur->son[x];
+            cur->pre--;
+        }
+        cur->endcnt--;
+    }
+};
