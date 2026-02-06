fb5ed70cfc0538889bc2ca0294bfaa8dc443509d
elainafan <869812580@qq.com>
2026-02-06 16:46:45 +0800

    update unionfind

--- 数据结构/UnionFind.cpp
+++ 数据结构/UnionFind.cpp
@@ -1,9 +1,9 @@
 class UnionFind {
     vector<int> fa;
-    vector<int> sz;  // 集合大小
+    vector<int> siz;  // 集合大小
 public:
     int cc;  // 连通块个数
-    UnionFind(int n) : fa(n), sz(n, 1), cc(n) { ranges::iota(fa, 0); }
+    UnionFind(int n) : fa(n), siz(n, 1), cc(n) { ranges::iota(fa, 0); }
     int get(int x) {
         if (fa[x] != x) fa[x] = get(fa[x]);
         return fa[x];
@@ -13,11 +13,11 @@ public:
         int x = get(from), y = get(to);
         if (x == y) return false;
         fa[x] = y;
-        sz[y] += sz[x];
+        siz[y] += siz[x];
         cc--;
         return true;
     }
     int get_size(int x) {  // 查询x所在集合大小
-        return sz[get(x)];
+        return siz[get(x)];
     }
 };
