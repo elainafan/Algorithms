d27dc9631489d57fff1f80798c8b4b59f392adfe
elainafan <869812580@qq.com>
2026-02-09 09:55:49 +0800

    update

--- 数学/Divisors.cpp
+++ 数学/Divisors.cpp
@@ -1,8 +1,8 @@
-constexpr int MX=2e5+5;
-vector<int>divisors[MX];
-auto init=[]{
-    for(int i=1;i<MX;i++){
-        for(int j=i;j<MX;j+=i){
+constexpr int MX = 2e5 + 5;
+vector<int> divisors[MX];
+auto init = [] {
+    for (int i = 1; i < MX; i++) {
+        for (int j = i; j < MX; j += i) {
             divisors[j].push_back(i);
         }
     }
