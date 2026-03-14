11d022e13340064e9c6d3720e730c10afdc87197
elainafan <869812580@qq.com>
2026-03-14 10:52:32 +0800

    update Sieve_Eu

--- C++/数学/Sieve_Eu.cpp
+++ C++/数学/Sieve_Eu.cpp
@@ -1,11 +1,11 @@
-constexpr int MX = 1e8 + 1;
+constexpr int MX = 6e6 + 1;
 bool pd[MX];
 vi primes;
 auto init = [] {
-    rep(i, 2, MX) {
+    rep(i, 2, MX - 1) {
         if (!pd[i]) primes.push_back(i);
         for (int& p : primes) {
-            if (p * i > MX) break;
+            if (1LL * p * i >= MX) break;
             pd[i * p] = true;
             if (i % p == 0) break;
         }
