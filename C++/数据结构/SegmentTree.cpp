5b137a65564875b58734bf7e9e682680e3a9b240
elainafan <869812580@qq.com>
2026-04-29 21:09:37 +0800

    update seg

--- C++/数据结构/SegmentTree.cpp
+++ C++/数据结构/SegmentTree.cpp
@@ -50,7 +50,7 @@ class SegmentTree {
         int res = find_first(node << 1, l, m, ql, qr, val);
         if (res != -1) return res;
         return find_first(node << 1 | 1, m + 1, r, ql, qr, val);
-    } // 若遇到固定左端点的情况，需要使用全局变量（或者传入引用）记录前缀分段最大值，加一个被待求区间完全覆盖的剪枝
+    }  // 若遇到固定左端点的情况，需要使用全局变量（或者传入引用）记录前缀分段最大值，加一个被待求区间完全覆盖的剪枝
 
     int find_last(int node, int l, int r, int ql, int qr, T val) const {
         if (r < ql || l > qr) return -1;
@@ -73,7 +73,137 @@ public:
 
     T get(int i) const { return query(1, 0, n - 1, i, i); }  // 取出i处的值
 
-    int find_first(int ql, int qr, T val) const { return find_first(1, 0, n - 1, ql, qr, val); } // 查询[ql,qr]中第一个满足条件的下标
+    int find_first(int ql, int qr, T val) const { return find_first(1, 0, n - 1, ql, qr, val); }  // 查询[ql,qr]中第一个满足条件的下标
 
-    int find_last(int ql, int qr, T val) const { return find_last(1, 0, n - 1, ql, qr, val); } // 查询[ql,qr]中最后一个满足条件的下标
+    int find_last(int ql, int qr, T val) const { return find_last(1, 0, n - 1, ql, qr, val); }  // 查询[ql,qr]中最后一个满足条件的下标
 };
+
+// CF1906F
+// 引入了维护Info，重载加法的新想法
+// 维护最大子段和，离线处理，换维度
+// 好题
+struct Info {
+    ll sum, pre, suf, ans;
+    Info(ll x = 0) : sum(x), pre(x), suf(x), ans(x) {}
+};
+Info operator+(const Info& a, const Info& b) {
+    Info c;
+    c.sum = a.sum + b.sum;
+    c.pre = max(a.pre, a.sum + b.pre);
+    c.suf = max(b.suf, b.sum + a.suf);
+    c.ans = max({a.ans, b.ans, a.suf + b.pre});
+    return c;
+}
+template <typename T>
+class SegmentTree {
+    int n;
+    vector<T> tree;
+    T merge_val(T a, T b) const { return a + b; }  // 合并子树
+
+    void maintain(int node) {  // 维护整棵树
+        tree[node] = merge_val(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void build(const vector<T>& a, int node, int l, int r) {
+        if (l == r) {
+            tree[node] = a[l];
+            return;
+        }
+        int m = (l + r) / 2;
+        build(a, node * 2, l, m);
+        build(a, node * 2 + 1, m + 1, r);
+        maintain(node);
+    }  // 建树
+
+    void update(int node, int l, int r, int i, T val) {
+        if (l == r) {
+            tree[node] = val;
+            return;
+        }
+        int m = (l + r) / 2;
+        if (i <= m)
+            update(node * 2, l, m, i, val);
+        else
+            update(node * 2 + 1, m + 1, r, i, val);
+        maintain(node);
+    }  // 更新i处的值为val
+
+    T query(int node, int l, int r, int ql, int qr) const {
+        if (ql <= l && r <= qr) return tree[node];
+        int m = (l + r) / 2;
+        if (qr <= m) return query(node * 2, l, m, ql, qr);
+        if (ql > m) return query(node * 2 + 1, m + 1, r, ql, qr);
+        T l_res = query(node * 2, l, m, ql, qr);
+        T r_res = query(node * 2 + 1, m + 1, r, ql, qr);
+        return merge_val(l_res, r_res);
+    }  // 查询[ql,qr]的值
+
+    int find_first(int node, int l, int r, int ql, int qr, T val) const {
+        if (r < ql || l > qr) return -1;
+        if (tree[node] < val) return -1;
+        if (l == r) return l;
+        int m = (l + r) >> 1;
+        int res = find_first(node << 1, l, m, ql, qr, val);
+        if (res != -1) return res;
+        return find_first(node << 1 | 1, m + 1, r, ql, qr, val);
+    }  // 若遇到固定左端点的情况，需要使用全局变量（或者传入引用）记录前缀分段最大值，加一个被待求区间完全覆盖的剪枝
+
+    int find_last(int node, int l, int r, int ql, int qr, T val) const {
+        if (r < ql || l > qr) return -1;
+        if (tree[node] < val) return -1;
+        if (l == r) return l;
+        int m = (l + r) >> 1;
+        int res = find_last(node << 1 | 1, m + 1, r, ql, qr, val);
+        if (res != -1) return res;
+        return find_last(node << 1, l, m, ql, qr, val);
+    }
+
+public:
+    SegmentTree(int n, T init_val) : SegmentTree(vector<T>(n, init_val)) {}
+
+    SegmentTree(const vector<T>& a) : n(a.size()), tree(2 << bit_width(a.size() - 1)) { build(a, 1, 0, n - 1); }  // 传入一个数组维护
+
+    void update(int i, T val) { update(1, 0, n - 1, i, val); }  // 更新i的值为val
+
+    T query(int ql, int qr) const { return query(1, 0, n - 1, ql, qr); }  // 查询[ql,qr]的值
+
+    T get(int i) const { return query(1, 0, n - 1, i, i); }  // 取出i处的值
+
+    int find_first(int ql, int qr, T val) const { return find_first(1, 0, n - 1, ql, qr, val); }  // 查询[ql,qr]中第一个满足条件的下标
+
+    int find_last(int ql, int qr, T val) const { return find_last(1, 0, n - 1, ql, qr, val); }  // 查询[ql,qr]中最后一个满足条件的下标
+};
+void solve() {
+    ll n, m, x, y, z, q;
+    cin >> n >> m;
+    vector<trl> ma(m);
+    vector<vector<pll>> event(n + 1);
+    rep(i, 0, m - 1) {
+        cin >> x >> y >> z;
+        x--, y--;
+        ma[i] = {x, y, z};
+        event[x].emplace_back(i, z);
+        if (y + 1 < n) event[y + 1].emplace_back(i, -z);
+    }
+    cin >> q;
+    vl ans(q);
+    vector<vector<trl>> ma2(n);
+    rep(i, 0, q - 1) {
+        cin >> x >> y >> z;
+        x--, y--, z--;
+        ma2[x].emplace_back(y, z, i);
+    }
+    SegmentTree<Info> tree(m, Info(0));
+    vl cur(m);
+    rep(i, 0, n - 1) {
+        for (auto& [x, y] : event[i]) {
+            cur[x] += y;
+            tree.update(x, Info(cur[x]));
+        }
+        for (auto& [x, y, z] : ma2[i]) {
+            ans[z] = tree.query(x, y).ans;
+        }
+    }
+    rep(i, 0, q - 1) cout << ans[i] << endl;
+    return;
+}
